74f016540cb56e32b01efcf2804e6e3e4a9568b6
alessiotagab <126098685+alessiotagab@users.noreply.github.com>
2023-03-18 19:24:33 +0100

    Update Color.java

--- src/main/java/Color.java
+++ src/main/java/Color.java
@@ -1,2 +1,10 @@
 public enum Color {
+Green,
+Pink,
+Blue,
+LightBlue,
+White,
+Yellow,
+Grey, //Grey caselle corrispondenti a tessere prese
+Black; //Caselle Black irraggiungibili della plancia iniziale  
 }
