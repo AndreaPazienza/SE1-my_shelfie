62d89769e8393a347b0285d67d88d2ea34e64dc8
alessiotagab <alessio.tagab@mail.polimi.it>
2023-03-20 21:50:07 +0100

    Classe slot

--- src/main/java/Slot.java
+++ src/main/java/Slot.java
@@ -1,2 +1,25 @@
 public class Slot {
+    private Color color;
+    private boolean catchable;
+
+    public Slot(Color color){
+        this.color=color;
+    }
+
+    public boolean isCatchable() {
+        return catchable;
+    }
+
+    public void setCatchable(boolean catchable) {
+        this.catchable = catchable;
+    }
+
+    public void setGrey(){ // la tessera è stata presa
+        this.color=Color.GREY;
+    }
+
+    public Color getColor() {
+        return color;
+    }
 }
+
