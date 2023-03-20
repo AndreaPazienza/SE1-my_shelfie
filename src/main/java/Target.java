1fc68d61dc5e5b659643f3029e0d3a20cb3841da
GabrieleProverbio <126098237+GabrieleProverbio@users.noreply.github.com>
2023-03-20 14:49:13 +0100

    Update Target.java

--- src/main/java/Target.java
+++ src/main/java/Target.java
@@ -1,2 +1,29 @@
 public class Target {
+    private Color tile;
+    private int PosX;
+    private int PosY;
+
+    public int getPosX() {
+        return PosX;
+    }
+
+    public int getPosY() {
+        return PosY;
+    }
+
+    public Color getTile() {
+        return tile;
+    }
+
+    public void setPosX(int x) {
+        this.PosX = x;
+    }
+
+    public void setPosY(int y) {
+        this.PosY = y;
+    }
+
+    public void setTile(Color color) {
+        this.tile = color;
+    }
 }
