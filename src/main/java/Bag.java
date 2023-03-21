120e8fec9da14394ed00efb947ed72788a0fd836
AndreaPazienza <andrea.pazienza@mail.polimi.it>
2023-03-21 14:48:03 +0100

    Classe Game e classe Bag (non complete)

--- src/main/java/Bag.java
+++ src/main/java/Bag.java
@@ -1,2 +1,27 @@
 public class Bag {
+
+    private Slot[] inBag;
+
+    //Costrutttore di Bag come array ordinato di Slot (l'estrazione randomica è lasciata al metodo Refill di Dashboard)
+    public Bag () {
+
+        int i = 0;
+
+        inBag = new Slot[132];
+
+        for (i = 0; i <132; i ++) {
+            inBag[i] = new  Slot();
+            inBag[i].setColore (i % 6);
+            inBag[i].setCatchable (true);
+        }
+    }
+
+    //Get e Set degli attributi privati
+    public Slot[] getInBag() {
+        return inBag;
+    }
+
+    public void setInBag(Slot[] inBag) {
+        this.inBag = inBag;
+    }
 }
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,90 @@
 public class Game {
+
+    private int Nplayers;
+    private boolean gameOn;
+    private Player[] player;
+    private Dashboard table;                            //Manca dashboard tra le classi
+    private Bag bag;
+    private CommonGoal commonGoal;
+
+    //Costruttore della partita che a sua volta costruisce la Dashboard passando
+    public Game () {
+
+        Nplayers = 0;
+        gameOn = true;
+        player = new Player[4];
+        table = new Dashboard();                        //Problema relativo al costruttore di Dashboard (numero dei giocatori)
+        bag = new Bag();
+        commonGoal = new CommonGoal();
+    }
+
+    //Inserimento del giocatore nell'array dei player e incremento di Nplayers
+    public void signPlayer(Player player) {
+        this.player[this.Nplayers] = player;
+        this.Nplayers ++;
+    }
+
+    //
+    public void PlayerOnStage() {
+        //Da definire
+    }
+
+    //
+    public void updateTurn() {
+        //Da definire
+    }
+
+    //
+    public void FinalScore() {
+        //Da definire
+    }
+
+    //Get e Set degli attributi privati
+    public int getNplayers() {
+        return Nplayers;
+    }
+
+    public boolean isGameOn() {
+        return gameOn;
+    }
+
+    public Player[] getPlayer() {
+        return player;
+    }
+
+    public Dashboard getTable() {
+        return table;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+
+    public CommonGoal getCommonGoal() {
+        return commonGoal;
+    }
+
+    public void setNplayers(int nplayers) {
+        Nplayers = nplayers;
+    }
+
+    public void setGameOn(boolean gameOn) {
+        this.gameOn = gameOn;
+    }
+
+    public void setPlayer(Player[] player) {
+        this.player = player;
+    }
+
+    public void setTable(Dashboard table) {
+        this.table = table;
+    }
+
+    public void setBag(Bag bag) {
+        this.bag = bag;
+    }
+
+    public void setCommonGoal(CommonGoal commonGoal) {
+        this.commonGoal = commonGoal;
+    }
 }
