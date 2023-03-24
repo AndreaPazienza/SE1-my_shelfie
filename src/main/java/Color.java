2f9b807bef54e9175453a2dc6cd29565568e8272
GabrieleProverbio <gabriele.proverbio@mail.polimi.it>
2023-03-24 18:23:12 +0100

    Update PersonalShelf.java (aggiunta metodo get della singola slot)

--- src/main/java/Color.java
+++ src/main/java/Color.java
@@ -1,38 +1,36 @@
 import java.util.Random;
 
 public enum Color {
-GREEN,
-PINK,
-BLUE,
-LIGHTBLUE,
-WHITE,
-YELLOW,
-GREY, //Grey caselle corrispondenti a tessere prese
-BLACK; //Caselle Black irraggiungibili della plancia iniziale
+    GREEN,
+    PINK,
+    BLUE,
+    LIGHTBLUE,
+    WHITE,
+    YELLOW,
+    GREY, //Grey caselle corrispondenti a tessere prese
+    BLACK; //Caselle Black irraggiungibili della plancia iniziale
 
     public static Color RandomColor() { //Generatore di colore casuale, esculdendo GREY e BLACK che non sono colori effettivi
         Color[] values = Color.values();
-        int length = (values.length-2);
+        int length = (values.length - 2);
         int randIndex = new Random().nextInt(length);
         return values[randIndex];
 
     }
 
     public boolean Equals(Object obj) {
-        boolean uguale;
-        if(obj == this){
+        if (obj == this) {
             return true;
-        }
-        else {
-            return false;
-    }
-}
-    public boolean notEquals(Object obj) {
-        if(obj != this){
-            return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
 
+    public boolean notEquals(Object obj) {
+        if (obj != this) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -44,101 +44,51 @@ public class Player {
         this.orderInTurn = order;
     }
 
-    public void setScore(int points) {
-        this.score = points;
+    public static void setScore(int points) {
+        score = points;
     }
 
     public void setShelf(PersonalShelf shelf) {
         this.shelf = shelf;
     }
 
-    public Slot[] selectCard (Dashboard dashboard) {
-        Slot[] selectedCards = new Slot[3];
-        int i = 0;
-        int x = 0;
-        int y = 0;
-        String yes = "si";
-        String no = "no";
-        do {  //il dw più esterno serve a ripetere il tutto per ogni scelta
-            do {  //questi do while servono a fare controllo delle cordinate in input
-                System.out.println("Scegli la tessera da prendere (inserire le coordinate): x = ");
-                Scanner scanner = new Scanner(System.in);
-                x = scanner.nextInt();
-                if (x > this.shelf.N_ROWS || x < 0) {
-                    System.out.println("La posizione scelta non esiste!");
-                }
-            } while (x > this.shelf.N_ROWS|| x < 0);
-            do {
-                System.out.println("y = ");
-                Scanner scanner = new Scanner(System.in);
-                y = scanner.nextInt();
-                if (y < 0 || y > this.shelf.N_COLUMN) {
-                    System.out.println("La posizione scelta non esiste!");
-                }
-            } while (y < 0 || y > this.shelf.N_COLUMN);
-            if (dashboard[x][y].isCatchable) {  //selezione vera e propria
-                selectedCards[i] = dashboard[x][y];
-                System.out.println("Vuoi scegliere altre tessere? si o no?");
-                Scanner scanner = new Scanner(System.in);
-                String stringa = scanner.next();
-                if(stringa.equals(yes)){
-                    i++;                //se vuole ancora scegliere, incremento solo i
-                } else if (stringa.equals(no)) {
-                    i = 3;              //se non vuole più scegliere, assegno i ad un valore maggiore del flag 2
-                } else {
-                    System.out.println("Non ho capito il comando!");
-                }
-            } else {
-                System.out.println("La tessera selezionata non è prendibile! ");
+    public Slot selectCard (Dashboard dashboard, int x, int y) {
+        Slot selectedCard = new Slot(Color.GREY);
+        if (dashboard[x][y].isCatchable) {  //selezione vera e propria
+            selectedCard = dashboard[x][y];
+            dashboard[x][y].setGrey();
             }
-        } while(i <= 2);
-        return selectedCards;
+        return selectedCard;
     }
 
-    public void orderCards(Slot[] selectedCards) {
-        int i = 0;
-        int j = 0;
-        int realLength = 0;
-        for(int k = 0;k < (selectedCards.length-1);k++){
-            if(selectedCards[i].getColor().notEquals(Color.GREY)){
-                realLength++;        //lunghezzaReale serve perchè in ingressi sarà dato un array di 3 slot
-            }                            //che non sempre sarà pieno;
-        }
-        Slot[] tmp = new Slot[3];       //variabile temporanea che serve per il riordino
-        do{
-            do{                     //questo do while mi serve a controllare l'input dell'utente: scova eventuali errori
-                System.out.println("Quale tessera vuoi inserire?");
-                Scanner scanner = new Scanner(System.in);
-                i = scanner.nextInt();
-                if(i < 1 || i > realLength){
-                    System.out.println("La tessera scelta non esiste!");
-                }
-                if(selectedCards[i-1] == null){
-                    System.out.println("La tessera scelta è già stata selezionata!");
-                }
-            } while(i < 1 || i > realLength || selectedCards[i-1] == null);
-            tmp[j] = selectedCards[i-1];
-            selectedCards[i-1] = null;
-            j++;
-        } while(realLength < j); //in questo do while più esterno creo un array temporaneo con le tessere ordinate come vuole il giocatore
-        for(i = 0; i < realLength; i++){
-            selectedCards[i] = tmp[i]; //copio tmp dentro selcards: ho l'array ordinato
+    public void orderCards(Slot[] selectedCards, int p, int s, int t) { //p è la posizione nell'array di partenza della prima tessera dell'array riordinato
+                                                                        //s è la seconda, t la terza
+        Slot[] tmp = new Slot[3];
+        tmp[0] = selectedCards[p];
+        tmp[1] = selectedCards[s];
+        tmp[2] = selectedCards[t];
+        for(int i = 0; i < 3; i++){
+            selectedCards[i] = tmp[i];
         }
     }
 
-    public int sumPoints(int p){
+    public void orderCards(Slot[] selectedCards){ //metodo che riordina nel caso in cui ho scelto solo due tessere
+        Slot tmp = selectedCards[1];
+        selectedCards[1] = selectedCards[0];
+        selectedCards[0] = tmp;
+    }
+
+    public void sumPoints(int p){
         int points = Player.getScore()+p;
-        return points;
+        Player.setScore(points);
     }
 
     public int checkScore(){
         int points = 0;
         int pgoalPoints = pgoal.AssignPoint(shelf);
-        int sgoal1Points =;
-        int sgoal2Points =;
-        int nearbyPoints = 0;
-
-        points = pgoalPoints+sgoal1Points+sgoal2Points+nearbyPoints; //idea: calcolo i 3 singoli punteggi e li sommo insieme
+        int sgoalsPoints = Player.getScore(); //i punti degli sharedgoals sono già dentro score (metodo give points)
+        int nearbyPoints = this.shelf.calculatePoints();
+        points = pgoalPoints+sgoalsPoints+nearbyPoints; //idea: calcolo i 3 singoli punteggi e li sommo insieme
         return points;
     }
 
@@ -147,5 +97,7 @@ public class Player {
         this.score = 0;
         this.shelf = new PersonalShelf();
         this.pgoal = new PersonalGoal();
+        this.orderInTurn = 0;
+        this.chair = false;
     }
 }
