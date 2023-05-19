b62252df6dd9ab903067d76711d5ed10b2b674ab
Rubin <rubin.ronchieri@mail.polimi.it>
2023-05-19 16:32:47 +0200

    Beta v1.04: bug fix per distribuzione

--- src/main/java/AppClient.java
+++ src/main/java/AppClient.java
@@ -1,39 +1,22 @@
-import Distributed.ServerRMIInterface;
 import Distributed.rmi.Client;
+import Distributed.ServerRMIInterface;
+import Distributed.rmi.ServerImpl;
 import Errors.SameNicknameException;
-import VIEW.GraphicInterface;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
-import static javafx.application.Application.launch;
+public class AppClient {
 
-public class AppClient extends Application {
-     Stage stage;
-
-    public static void main(String[] args) throws Exception {
-        launch(args);
-    }
-
-    public void start (Stage stage) throws Exception{
-
-        this.stage = stage;
-
+    public static void main(String[] args) throws RemoteException, NotBoundException, SameNicknameException {
         // Per implementazione del online.
-        //Registry registry = LocateRegistry.getRegistry("192.168.22.214",1066);
-        Registry registry = LocateRegistry.getRegistry(1066);
-        ServerRMIInterface server = (ServerRMIInterface) registry.lookup("server");
+        Registry registry = LocateRegistry.getRegistry("192.168.22.214",1066);
+        // Registry registry = LocateRegistry.getRegistry(1066);
+       ServerRMIInterface server = (ServerRMIInterface) registry.lookup("server");
 
-        Client client = new Client(server,stage);
-
-
+        Client client = new Client(server);
+        client.run();
     }
-
 }
