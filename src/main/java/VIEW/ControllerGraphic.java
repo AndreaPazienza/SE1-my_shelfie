f4cca0a2bb76e5199993adeecbaee2c682c2e087
alessiotagab <alessio.tagab@mail.polimi.it>
2023-06-21 22:45:53 +0200

    Aggiunta schermate di gioco piu modifica di quelle pre esistenti

--- src/main/java/VIEW/ControllerGraphic.java
+++ src/main/java/VIEW/ControllerGraphic.java
@@ -1,15 +1,38 @@
 package VIEW;
 
+import Errors.SameNicknameException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 
 public class ControllerGraphic {
-    
-    
+    TextField inserimentoNick;
 
+    @FXML
     public void ConfermaScelta(javafx.event.ActionEvent actionEvent) {
+
+
     }
 
     public void GetText(ActionEvent actionEvent) {
+        try {
+            TextField inserimentoNick;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    public void Return2(ActionEvent actionEvent) {
+
+    }
+
+    public void Return3(ActionEvent actionEvent) {
+    }
+
+    public void Return4(ActionEvent actionEvent) {
+    }
+
+
+
 }
