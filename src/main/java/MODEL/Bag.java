270c862bbebb259ec88337a69f0b890a3defe5cd
AndreaPazienza <andrea.pazienza@mail.polimi.it>
2023-06-28 16:57:39 +0200

    Beta v1.10.2

--- src/main/java/MODEL/Bag.java
+++ src/main/java/MODEL/Bag.java
@@ -4,6 +4,9 @@ import java.util.Comparator;
 import java.util.Observable;
 import java.util.Random;
 
+/**
+ * Class that represents the bag of slots, ordered by color.
+ */
 public class Bag{
 
     private ArrayList<Slot> inBag = new ArrayList<>();
