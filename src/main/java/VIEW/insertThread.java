27a91d95911b16c35bd38c21b3b6538773df133b
AndreaPazienza <andrea.pazienza@mail.polimi.it>
2023-06-29 11:30:07 +0200

    Commenti JavaDoc package View (parte 1)

--- src/main/java/VIEW/insertThread.java
+++ src/main/java/VIEW/insertThread.java
@@ -6,15 +6,34 @@ import Errors.NotEnoughSpaceChoiceException;
 
 import java.rmi.RemoteException;
 
+
+/**
+ * Class that manages the insert phase of the turn.
+ */
 public class insertThread extends Thread{
 
+    /**
+     * The value that manage the thread.
+     */
     public volatile boolean valid = true;
+
+    /**
+     * The interface of the current player.
+     */
     private GameInterface view;
 
+    /**
+     * Constructor for insertThread.
+     *
+     * @param view The interface of the current player.
+     */
     public insertThread(GameInterface view){
         this.view = view;
     }
 
+    /**
+     * Runs the thread while is valid to manage the insert phase of the turn.
+     */
     @Override
     public synchronized void run() {
       while(valid) {
@@ -29,7 +48,9 @@ public class insertThread extends Thread{
          }
       }
 
-
+    /**
+     * Sets value on false to stop the thread.
+     */
     public void stopThread(){
         valid = false;
     }
